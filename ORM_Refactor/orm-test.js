d37baf79377f64a9f9f81e894ed15fb610727c29
William Ngo <will@mrngoitall.net>
2013-08-31 15:51:58 -0700

    Updated ORM test

--- ORM_Refactor/orm-test.js
+++ ORM_Refactor/orm-test.js
@@ -4,20 +4,50 @@ var mysql = require('mysql');
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize("orm_chat", "root", "fzPJlqy2fLs7srDER6LRx9eo8");
 
-var Message = sequelize.define('Message', { 
-  user: Sequelize.STRING, 
-  room: Sequelize.STRING,
-  message: Sequelize.STRING });
+// var Message = sequelize.define('Message', { 
+//   user: Sequelize.STRING, 
+//   room: Sequelize.STRING,
+//   message: Sequelize.STRING });
 
-//sequelize.sync({force:true});
-sequelize.sync();
+var User = sequelize.define('User', { name: Sequelize.STRING });
+var Room = sequelize.define('Room', { name: Sequelize.STRING });
+var Message = sequelize.define('Message', { message: Sequelize.STRING });
 
-var message = Message.build({
-  user: 'Will',
-  room: 'messages',
-  message: "Damn sequelize. Why can't you be more flexible? :("
+Message.hasOne(User);
+Message.hasOne(Room);
+
+User.hasMany(Message);
+Room.hasMany(Message);
+
+Message.belongsTo(User);
+Message.belongsTo(Room);
+
+sequelize.sync({force:true}).success(function() {
+  var user = User.create({
+    name: 'Will'
+  }).success(function() {
+    console.log("Saved user.");
+  });;
+
+  var room = Room.create({
+    name: 'messages'
+  }).success(function() {
+    console.log("Saved room.");
+  });
+
+  var message = Message.create({
+    message: 'Test message'
+  });
+
+  message.setUser(user).success(function() {
+    console.log("Associated user with message.");
+  });
+  message.setRoom(room).success(function() {
+    console.log("Associated room with message.");
+  });
+
+  message.save().success(function() {
+    console.log('Done saving!');
+  });
 });
 
-message.save().success(function() {
-  console.log('done!');
-});
