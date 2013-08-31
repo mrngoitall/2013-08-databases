2a071051b1ff89974ea6a3e299f49bec1db5286e
William Ngo <will@mrngoitall.net>
2013-08-31 16:26:57 -0700

    Refactored to use express

--- SQL/request-handler.js
+++ SQL/request-handler.js
@@ -8,12 +8,6 @@ var dbConnection = mysql.createConnection({
 });
 dbConnection.connect();
 
-/* You should implement your request handler function in this file.
- * But you need to pass the function to http.createServer() in
- * basic-server.js.  So you must figure out how to export the function
- * from this file and include it in basic-server.js. Check out the
- * node module documentation at http://nodejs.org/api/modules.html. */
-
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -30,51 +24,7 @@ exports.handleRequest = function(request, response) {
   var parsedURL = url.parse(request.url).pathname.split("/");
   console.log("Parsed URL: ", parsedURL);
 
-  if (request.url === '/js/setup.js') {
-    fs.readFile(__dirname + '/js/setup.js',
-      function (err, data) {
-        if (err) {
-          response.writeHead(500);
-          return response.end('Error loading setup.js');
-        }
-
-        response.writeHead(200,{ 'Content-Type': 'text/javascript' });
-        response.end(data);
-      });
-  } else if (request.url === '/css/styles.css') {
-    fs.readFile(__dirname + '/css/styles.css',
-      function (err, data) {
-        if (err) {
-          response.writeHead(500);
-          return response.end('Error loading styles.css');
-        }
-
-        response.writeHead(200,{ 'Content-Type': 'text/css' });
-        response.end(data);
-      });
-  } else if (request.url === '/css/reset.css') {
-    fs.readFile(__dirname + '/css/reset.css',
-      function (err, data) {
-        if (err) {
-          response.writeHead(500);
-          return response.end('Error loading reset.css');
-        }
-
-        response.writeHead(200,{ 'Content-Type': 'text/css' });
-        response.end(data);
-      });
-  } else if (request.url === '/index.html' || request.url === '/') {
-    fs.readFile(__dirname + '/index.html',
-      function (err, data) {
-        if (err) {
-          response.writeHead(500);
-          return response.end('Error loading index.html');
-        }
-
-        response.writeHead(200,{ 'Content-Type': 'text/html' });
-        response.end(data);
-      });
-  } else if (request.method === 'POST') {
+  if (request.method === 'POST') {
     sendMessageHandler(request, response, parsedURL[2]);
   } else if (request.method === 'GET' && parsedURL[1] === 'classes') {
     getMessagesHandler(request, response, parsedURL[2]);
